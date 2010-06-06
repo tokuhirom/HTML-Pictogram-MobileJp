f7194fed02d74e9a26a4862af5f767fc45215c60
tokuhirom <tokuhirom@gmail.com>
2010-06-06 10:04:45 +0900

    initial import

---
+++ lib/HTML/Pictogram/MobileJp.pm
@@ -0,0 +1,39 @@
+package HTML::Pictogram::MobileJp;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+HTML::Pictogram::MobileJp -
+
+=head1 SYNOPSIS
+
+  use HTML::Pictogram::MobileJp;
+
+=head1 DESCRIPTION
+
+HTML::Pictogram::MobileJp is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
